163a84fb84235c4d87723bf0083ec204f5220121
Mawin's MAC <ufund@Mawins-Mac.local>
2025-05-27 07:29:35 +0700

    Fix logic in A1-025.cpp: improve card face and suit handling

--- A1/A1-025.cpp
+++ A1/A1-025.cpp
@@ -1,38 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    string str;
-    cin >> str;
-    string rank;
-    char suit;
-    if (str.length() == 2)
-        rank = str[0];
-    else
-        rank = str.substr(0, 2);
-    suit = str.back();
-
-    string arrRank[14] = {"", "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
-    int n;
-    if (rank == "A" || rank == "a")
-        n = 1;
-    else if (rank == "J" || rank == "j")
-        n = 11;
-    else if (rank == "Q" || rank == "q")
-        n = 12;
-    else if (rank == "K" || rank == "k")
-        n = 13;
-    else
-        n = stoi(rank);
-    cout << arrRank[n] << " of ";
-
-    if (suit == 'D' || suit == 'd')
-        cout << "diamonds";
-    else if (suit == 'H' || suit == 'h')
-        cout << "hearts";
-    else if (suit == 'S' || suit == 's')
-        cout << "spades";
-    else
-        cout << "clubs";
-    return 0;
+int main() {
+    string s;
+    cin >> s;
+    char k = s.back();
+    s.pop_back();
+    if (s == "Q" || s == "q") cout << "queen";
+    else if (s == "J" || s == "j") cout << "jack";
+    else if (s == "A" || s == "a") cout << "ace";
+    else if (s == "K" || s == "k") cout << "king";
+    else cout << s;
+    cout << " of ";
+    k = toupper(k);
+    if (k == 'D') cout << "diamonds";
+    else if (k == 'H') cout << "hearts";
+    else if (k == 'S') cout << "spades";
+    else cout << "clubs";
 }
