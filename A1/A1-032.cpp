8488f01dc501c5dbdcf14425e4ef99e08f3d19b0
Mawin's MAC <ufund@Mawins-Mac.local>
2025-05-27 07:30:47 +0700

    your message

--- A1/A1-032.cpp
+++ A1/A1-032.cpp
@@ -18,16 +18,3 @@ int main()
 
     return 0;
 }
-
-// int n;cin>>n;
-// for(int i=0;i<n;i++)
-//     cout<<"*";
-// cout<<endl;
-// if(n-2>=1)
-//     for(int i=0;i<n-2;i++)
-//     cout<<"*";
-// cout<<endl;
-// if(n-4>=1)
-//     for(int i=0;i<n-4;i++)
-//     cout<<"*";
-// return 0;
